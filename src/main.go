55ca15423a35240f048930125526f3d5ec2c94bf
Pavel Zabelin <zabelin.p@soft2bet.com>
2024-10-08 10:25:01 +0300

    Add projections and make refactoring

--- src/main.go
+++ src/main.go
@@ -12,7 +12,6 @@ import (
 	"github.com/vektah/gqlparser/v2/ast"
 )
 
-// Struct to store type and interface information
 type TypeInfo struct {
 	Name       string
 	Definition *ast.Definition
@@ -28,19 +27,17 @@ var (
 )
 
 func main() {
-	// Get command-line parameters
+
 	inputDir := flag.String("input", "./schemas", "Directory with GraphQL schemas")
 	outputPath := flag.String("output", "./generated-types.ts", "Path for the output TypeScript file")
 	flag.BoolVar(&skipChecks, "skipChecks", false, "Skip type mismatch checks")
 	flag.BoolVar(&debug, "debug", false, "Print debug log")
 	flag.Parse()
 
-	// Check if input directory exists
 	if _, err := os.Stat(*inputDir); os.IsNotExist(err) {
 		log.Fatalf("Input directory does not exist: %s", *inputDir)
 	}
 
-	// Read all .graphql files from the specified directory
 	err := filepath.Walk(*inputDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -60,7 +57,6 @@ func main() {
 		log.Fatalf("Error processing schema files: %v", err)
 	}
 
-	// Generate TypeScript file
 	if err := generateTypescriptFile(*outputPath); err != nil {
 		log.Fatalf("Error generating TypeScript file: %v", err)
 	}
@@ -74,9 +70,8 @@ func debugPrint(format string, a ...any) {
 	}
 }
 
-// Function to process a single GraphQL schema file
 func processSchemaFile(path string) error {
-	// Read the schema file
+
 	fileContent, err := os.ReadFile(path)
 	if err != nil {
 		return fmt.Errorf("could not read file %s: %v", path, err)
@@ -84,7 +79,6 @@ func processSchemaFile(path string) error {
 
 	debugPrint("Parsing file: %s\n", path)
 
-	// Parse the schema
 	schema, err := gqlparser.LoadSchema(&ast.Source{
 		Input: string(fileContent),
 	})
@@ -92,18 +86,17 @@ func processSchemaFile(path string) error {
 		return fmt.Errorf("error parsing schema in file %s: %v", path, err)
 	}
 
-	// Process types and interfaces
 	for _, typ := range schema.Types {
 		debugPrint("Processing type: %s from file %s\n", typ.Name, path)
 		if typ.Kind == ast.Object || typ.Kind == ast.Interface {
 			if typ.Name == "Query" {
-				// Добавляем все поля Query
+
 				for _, field := range typ.Fields {
 					debugPrint("Adding Query field: %s\n", field.Name)
 					queries[field.Name] = field
 				}
 			} else if typ.Name == "Mutation" {
-				// Добавляем все поля Mutation
+
 				for _, field := range typ.Fields {
 					debugPrint("Adding Mutation field: %s\n", field.Name)
 					mutations[field.Name] = field
@@ -116,7 +109,6 @@ func processSchemaFile(path string) error {
 			}
 		}
 
-		// Process enums
 		if typ.Kind == ast.Enum {
 			debugPrint("Processing enum: %s from file %s\n", typ.Name, path)
 			if err := addEnum(typ); err != nil {
@@ -129,16 +121,15 @@ func processSchemaFile(path string) error {
 	return nil
 }
 
-// Add type or interface to the global list
 func addTypeOrInterface(def *ast.Definition) error {
 	existing, found := types[def.Name]
 	if found {
-		// Compare type or interface structure if skipChecks is not enabled
+
 		if !skipChecks && !compareDefinitions(existing.Definition, def) {
 			return fmt.Errorf("error: type or interface %s has conflicting definitions", def.Name)
 		}
 	} else {
-		// Add new type or interface
+
 		types[def.Name] = &TypeInfo{
 			Name:       def.Name,
 			Definition: def,
@@ -147,11 +138,10 @@ func addTypeOrInterface(def *ast.Definition) error {
 	return nil
 }
 
-// Add enum to the global list
 func addEnum(enum *ast.Definition) error {
 	existingEnum, found := enums[enum.Name]
 	if found {
-		// Compare enums if skipChecks is not enabled
+
 		if !skipChecks && !compareEnums(existingEnum, enum) {
 			return fmt.Errorf("error: enum %s has conflicting definitions", enum.Name)
 		}
@@ -161,7 +151,6 @@ func addEnum(enum *ast.Definition) error {
 	return nil
 }
 
-// Compare the structures of two type or interface definitions
 func compareDefinitions(a, b *ast.Definition) bool {
 	if len(a.Fields) != len(b.Fields) {
 		return false
@@ -174,7 +163,6 @@ func compareDefinitions(a, b *ast.Definition) bool {
 	return true
 }
 
-// Compare the structures of two enums
 func compareEnums(a, b *ast.Definition) bool {
 	if len(a.EnumValues) != len(b.EnumValues) {
 		return false
@@ -187,16 +175,37 @@ func compareEnums(a, b *ast.Definition) bool {
 	return true
 }
 
-// Generate the final TypeScript file
-func generateTypescriptFile(outputPath string) error {
-	file, err := os.Create(outputPath)
-	if err != nil {
-		return fmt.Errorf("could not create file: %v", err)
-	}
-	defer file.Close()
+func generateRootInterfaces(buffer *strings.Builder, fields map[string]*ast.FieldDefinition, rootName string, deferredInterfaces map[string]bool, processedTypes map[string]bool) {
+	if len(fields) > 0 {
+		buffer.WriteString(fmt.Sprintf("export interface %s {\n", rootName))
+		for _, field := range fields {
+			isOptional := !strings.HasSuffix(field.Type.String(), "!")
+			fieldType := convertGraphqlTypeToTs(field.Type.String())
 
-	// Header
-	file.WriteString(`/*
+			if isOptional {
+				buffer.WriteString(fmt.Sprintf("  %s?: Nullable<%s>;\n", field.Name, fieldType))
+			} else {
+				buffer.WriteString(fmt.Sprintf("  %s: %s;\n", field.Name, fieldType))
+			}
+
+			bufferRequestInterface(field.Type.String(), processedTypes, deferredInterfaces)
+		}
+		buffer.WriteString("}\n\n")
+	}
+}
+
+func generateEnums(buffer *strings.Builder) {
+	for _, enum := range enums {
+		buffer.WriteString(fmt.Sprintf("export enum %s {\n", enum.Name))
+		for _, value := range enum.EnumValues {
+			buffer.WriteString(fmt.Sprintf("  %s = '%s',\n", value.Name, value.Name))
+		}
+		buffer.WriteString("}\n\n")
+	}
+}
+
+func writeFileHeader(buffer *strings.Builder) {
+	buffer.WriteString(`/*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
  * -------------------------------------------------------
@@ -206,85 +215,184 @@ func generateTypescriptFile(outputPath string) error {
 /* eslint-disable */
 
 `)
-	file.WriteString("type Nullable<T> = T | null;\n\n")
+	buffer.WriteString("type Nullable<T> = T | null;\n\n")
+}
+
+func isObjectType(typeName string) bool {
+	_, found := types[typeName]
+	return found
+}
+
+func extractCleanType(typeStr string) string {
+	cleanType := strings.TrimSuffix(typeStr, "!")
+	if strings.HasPrefix(cleanType, "[") && strings.HasSuffix(cleanType, "]") {
+		cleanType = cleanType[1 : len(cleanType)-1]
+		cleanType = strings.TrimSuffix(cleanType, "!")
+	}
+	return cleanType
+}
+
+func bufferMissingRequestInterfaces(deferredInterfaces map[string]bool) {
+	for typeName := range types {
+		// Проверяем, является ли тип объектом или массивом объектов
+		if isObjectOrArrayOfObjects(typeName) && !deferredInterfaces[typeName] {
+			// Добавляем его для дальнейшей обработки
+			deferredInterfaces[typeName] = true
+			bufferRequestInterface(typeName, make(map[string]bool), deferredInterfaces)
+		}
+	}
+}
+
+func generateTypeInterface(buffer *strings.Builder, typeInfo *TypeInfo) {
+	if typeInfo.Definition.Kind == ast.Object {
+		buffer.WriteString(fmt.Sprintf("export interface %s {\n", typeInfo.Name))
+	} else if typeInfo.Definition.Kind == ast.Interface {
+		buffer.WriteString(fmt.Sprintf("export interface %s {\n", typeInfo.Name))
+	}
+
+	for _, field := range typeInfo.Definition.Fields {
+		isOptional := !strings.HasSuffix(field.Type.String(), "!")
+		fieldType := convertGraphqlTypeToTs(field.Type.String())
+
+		if isOptional {
+			buffer.WriteString(fmt.Sprintf("  %s?: Nullable<%s>;\n", field.Name, fieldType))
+		} else {
+			buffer.WriteString(fmt.Sprintf("  %s: %s;\n", field.Name, fieldType))
+		}
+	}
+
+	buffer.WriteString("}\n\n")
+}
+
+func generateRequestInterfaces(buffer *strings.Builder, deferredInterfaces map[string]bool) {
+	for deferredType := range deferredInterfaces {
+		typeInfo, found := types[deferredType]
+		if found {
+			projectionInterfaceName := fmt.Sprintf("%sRequest", deferredType)
+			buffer.WriteString(fmt.Sprintf("export interface %s {\n", projectionInterfaceName))
+
+			for _, field := range typeInfo.Definition.Fields {
+				fieldTypeStr := field.Type.String()
+				isOptional := !strings.HasSuffix(fieldTypeStr, "!")
+				fieldType := determineFieldType(fieldTypeStr)
+
+				if isOptional {
+					buffer.WriteString(fmt.Sprintf("  %s?: %s;\n", field.Name, fieldType))
+				} else {
+					buffer.WriteString(fmt.Sprintf("  %s: %s;\n", field.Name, fieldType))
+				}
+			}
+
+			buffer.WriteString("}\n\n")
+		}
+	}
+}
+
+func determineFieldType(fieldTypeStr string) string {
+	cleanType := extractCleanType(fieldTypeStr)
+	fieldType := ""
+
+	if strings.HasPrefix(cleanType, "[") && strings.HasSuffix(cleanType, "]") {
+		innerType := cleanType[1 : len(cleanType)-1]
+		innerType = strings.TrimSuffix(innerType, "!")
+		if isObjectType(innerType) {
+			fieldType = fmt.Sprintf("Array<%sRequest>", innerType)
+		} else {
+			fieldType = "Array<boolean | number>"
+		}
+	} else if isObjectType(cleanType) {
+		fieldType = fmt.Sprintf("%sRequest", cleanType)
+	} else {
+		fieldType = "boolean | number"
+	}
+
+	return fieldType
+}
+
+func generateTypescriptFile(outputPath string) error {
+	file, err := os.Create(outputPath)
+	if err != nil {
+		return fmt.Errorf("could not create file: %v", err)
+	}
+	defer file.Close()
+
+	// Buffer to collect Request interfaces
+	var requestInterfacesBuffer strings.Builder
+	processedTypes := make(map[string]bool)
+	deferredInterfaces := make(map[string]bool)
+
+	// Header
+	writeFileHeader(&requestInterfacesBuffer)
 
 	// Generate enums in "mirror" style
-	for _, enum := range enums {
-		file.WriteString(fmt.Sprintf("export enum %s {\n", enum.Name))
-		for _, value := range enum.EnumValues {
-			file.WriteString(fmt.Sprintf("  %s = '%s',\n", value.Name, value.Name))
-		}
-		file.WriteString("}\n\n")
-	}
+	generateEnums(&requestInterfacesBuffer)
 
 	// Generate interfaces and types
 	for _, typeInfo := range types {
-		if typeInfo.Definition.Kind == ast.Object {
-			file.WriteString(fmt.Sprintf("export interface %s {\n", typeInfo.Name))
-		} else if typeInfo.Definition.Kind == ast.Interface {
-			file.WriteString(fmt.Sprintf("export interface %s {\n", typeInfo.Name))
-		}
+		generateTypeInterface(&requestInterfacesBuffer, typeInfo)
 
+		// "Помечаем" вложенные объекты для последующей генерации Request-интерфейсов
 		for _, field := range typeInfo.Definition.Fields {
-			isOptional := !strings.HasSuffix(field.Type.String(), "!")
-			fieldType := convertGraphqlTypeToTs(field.Type.String())
-			if isOptional {
-				file.WriteString(fmt.Sprintf("  %s?: Nullable<%s>;\n", field.Name, fieldType))
-			} else {
-				file.WriteString(fmt.Sprintf("  %s: %s;\n", field.Name, fieldType))
+			if isObjectOrArrayOfObjects(field.Type.String()) {
+				bufferRequestInterface(field.Type.String(), processedTypes, deferredInterfaces)
 			}
 		}
-
-		file.WriteString("}\n\n")
 	}
 
-	// Generate Query interface
-	if len(queries) > 0 {
-		file.WriteString("export interface Query {\n")
-		for _, query := range queries {
-			isOptional := !strings.HasSuffix(query.Type.String(), "!")
-			fieldType := convertGraphqlTypeToTs(query.Type.String())
-			if isOptional {
-				file.WriteString(fmt.Sprintf("  %s?: Nullable<%s>;\n", query.Name, fieldType))
-			} else {
-				file.WriteString(fmt.Sprintf("  %s: %s;\n", query.Name, fieldType))
-			}
-		}
-		file.WriteString("}\n\n")
-	}
+	// Generate Query and Mutation interfaces
+	generateRootInterfaces(&requestInterfacesBuffer, queries, "Query", deferredInterfaces, processedTypes)
+	generateRootInterfaces(&requestInterfacesBuffer, mutations, "Mutation", deferredInterfaces, processedTypes)
 
-	// Generate Mutation interface
-	if len(mutations) > 0 {
-		file.WriteString("export interface Mutation {\n")
-		for _, mutation := range mutations {
-			isOptional := !strings.HasSuffix(mutation.Type.String(), "!")
-			fieldType := convertGraphqlTypeToTs(mutation.Type.String())
-			if isOptional {
-				file.WriteString(fmt.Sprintf("  %s?: Nullable<%s>;\n", mutation.Name, fieldType))
-			} else {
-				file.WriteString(fmt.Sprintf("  %s: %s;\n", mutation.Name, fieldType))
-			}
-		}
-		file.WriteString("}\n\n")
-	}
+	// Добавляем недостающие Request интерфейсы для типов, которые не вошли в родительские интерфейсы
+	bufferMissingRequestInterfaces(deferredInterfaces)
+
+	// Write all deferred Request interfaces
+	generateRequestInterfaces(&requestInterfacesBuffer, deferredInterfaces)
+
+	// Write all buffered Request interfaces
+	file.WriteString(requestInterfacesBuffer.String())
 
 	return nil
 }
 
-// Convert GraphQL types to TypeScript types
+func bufferRequestInterface(returnType string, processedTypes map[string]bool, deferredInterfaces map[string]bool) {
+	cleanType := extractCleanType(returnType)
+
+	// Если тип уже обработан, пропускаем его
+	if processedTypes[cleanType] {
+		return
+	}
+	processedTypes[cleanType] = true
+
+	// Проверяем, существует ли такой тип среди объектов
+	typeInfo, found := types[cleanType]
+	if found {
+		// "Помечаем" тип для дальнейшей обработки
+		deferredInterfaces[cleanType] = true
+
+		// Рекурсивно обрабатываем поля
+		for _, field := range typeInfo.Definition.Fields {
+			if isObjectOrArrayOfObjects(field.Type.String()) {
+				bufferRequestInterface(field.Type.String(), processedTypes, deferredInterfaces)
+			}
+		}
+	}
+}
+
+func isObjectOrArrayOfObjects(fieldType string) bool {
+	cleanType := extractCleanType(fieldType)
+	_, found := types[cleanType]
+	return found
+}
+
 func convertGraphqlTypeToTs(graphqlType string) string {
-	// Remove '!' at the end, as this represents non-nullable type in GraphQL
 	cleanType := strings.TrimSuffix(graphqlType, "!")
 
-	// Check if this is an array
 	if strings.HasPrefix(cleanType, "[") && strings.HasSuffix(cleanType, "]") {
-		// This is an array, extract the inner type
 		innerType := cleanType[1 : len(cleanType)-1]
-		// Recursively call convertGraphqlTypeToTs for the inner type
 		return "Array<" + convertGraphqlTypeToTs(innerType) + ">"
 	}
 
-	// Convert standard GraphQL types to TypeScript types
 	switch cleanType {
 	case "String":
 		return "string"
@@ -295,13 +403,12 @@ func convertGraphqlTypeToTs(graphqlType string) string {
 	case "Boolean":
 		return "boolean"
 	case "ID":
-		return "string" // In TypeScript, IDs can be represented as strings
+		return "string"
 	case "DateTime":
 		return "string"
 	case "JSONObject":
 		return "Record<string, unknown>"
 	default:
-		// Keep custom types as they are
 		return cleanType
 	}
 }
